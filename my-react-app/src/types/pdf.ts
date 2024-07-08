c714192ff63faf4e0cd3b132d6773a780f5d5c45
Luis Fernando <luisfernandobr2003@gmail.com>
2024-07-08 04:09:40 -0300

    feat: add types

---
+++ my-react-app/src/types/pdf.ts
@@ -0,0 +1,10 @@
+export type PDF = {
+  file_name: string
+  file_size: number
+  images: string[]
+  most_common_word: string
+  text_content: string
+  total_images: number
+  total_words: number
+  word_frequency: number
+}
